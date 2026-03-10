5ba9234442d61d7e6ed28928e13ef9e4ae381f89
citor-tap <cit.tp.or@gmail.com>
2026-03-09 23:16:25 -0600

    Update main.asm

--- Esepedo/Esepedo/main.asm
+++ Esepedo/Esepedo/main.asm
@@ -8,4 +8,24 @@
 
 .equ N      = 100
 .equ MODVAL = 101
-cmdkvdvmv
+///////// MEMORIA SRAM (DATOS) /////////
+
+.dseg
+
+table_of_unsorted_numbers:      .byte N
+; Aquí se guardan los 100 números generados al azar
+
+table_of_sorted_numbers_alg1:   .byte N
+; Copia ordenada usando Bubble Sort
+
+table_of_sorted_numbers_alg2:   .byte N
+; Copia ordenada usando Insertion Sort
+
+rng_state:      .byte 1
+; Guarda el estado actual del generador pseudoaleatorio
+
+time_alg1_ticks:.byte 2
+; Tiempo que tarda Bubble Sort
+
+time_alg2_ticks:.byte 2
+; Tiempo que tarda Insertion Sort
