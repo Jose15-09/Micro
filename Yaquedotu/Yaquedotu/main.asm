1b75779e9184632c6cfbf60f815f3b2e16bab584
Jose15-09 <joseadrianfloresm@gmail.com>
2026-03-17 11:45:59 -0600

    Aquí empieza el código
    Set constants for the array size and the range of random values.

--- Yaquedotu/Yaquedotu/main.asm
+++ Yaquedotu/Yaquedotu/main.asm
@@ -1,1 +1,6 @@
+; 1. Aquí solo definimos algunos valores que vamos a usar en todo el programa
+; para no estar escribiendo números a cada rato
+
+.equ N      = 100        ; básicamente el arreglo tendrá 100 números
+.equ MODVAL = 101        ; esto se usa para que los números aleatorios queden entre 0 y 100
 
