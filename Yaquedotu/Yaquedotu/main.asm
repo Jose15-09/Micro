3f981347bfa5a20f4e77d470ed055d29b18eb4cc
citor-tap <cit.tp.or@gmail.com>
2026-03-17 11:49:50 -0600

    Allocate memory for sorting arrays and variables
    Added memory allocation for unsorted and sorted number arrays, random number generator state, and timing variables for sorting algorithms.

--- Yaquedotu/Yaquedotu/main.asm
+++ Yaquedotu/Yaquedotu/main.asm
@@ -3,4 +3,16 @@
 
 .equ N      = 100        ; básicamente el arreglo tendrá 100 números
 .equ MODVAL = 101        ; esto se usa para que los números aleatorios queden entre 0 y 100
+; 2. Empieza la parte de memoria RAM
+; Se aparta espacio para guardar todos los arreglos y variables
 
+.dseg
+
+table_of_unsorted_numbers:      .byte N   ; Se guardan los 100 números aleatorios sin ordenar
+table_of_sorted_numbers_alg1:   .byte N   ; Se guardará el arreglo ya ordenado con Bubble Sort
+table_of_sorted_numbers_alg2:   .byte N   ; Se guardará el arreglo ordenado con Selection Sort
+
+rng_state:                      .byte 1   ; Variable que guarda el estado del generador de números aleatorios
+
+time_alg1_ticks:                .byte 2   ; Se guarda el tiempo de Bubble Sort
+time_alg2_ticks:                .byte 2   ; Se guarda el tiempo de Selection Sort
